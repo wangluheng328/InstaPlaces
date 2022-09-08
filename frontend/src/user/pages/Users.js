7f1aad898f9913ec3cdcfe360e6e0f2600512f6a
wangluheng328 <57925878+wangluheng328@users.noreply.github.com>
2022-09-08 09:20:35 -0400

    Project Finished

---
+++ frontend/src/user/pages/Users.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react';
+
+import UsersList from '../components/UsersList';
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
+import { useHttpClient } from '../../shared/hooks/http-hook';
+import './Users.css';
+
+const Users = () => {
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+  const [loadedUsers, setLoadedUsers] = useState();
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const responseData = await sendRequest(
+          process.env.REACT_APP_BACKEND_URL + '/users'
+        );
+
+        setLoadedUsers(responseData.users);
+      } catch (err) {}
+    };
+    fetchUsers();
+  }, [sendRequest]);
+
+  return (
+    <React.Fragment>
+      <div className='alert'>
+          Alert: All images (avatars, photos) are deleted every 24 hours due to Heroku not having persistent storage.
+          Therefore, you might see most users' avatars and uploaded images not rendering. Everything works as desired 
+          within each 24-hour cycle. Will integrate AWS S3 to fix this issue soon.
+        </div>
+      <ErrorModal error={error} onClear={clearError} />
+      {isLoading && (
+        <div className="center">
+          <LoadingSpinner />
+        </div>
+      )}
+      {!isLoading && loadedUsers && <UsersList items={loadedUsers} />}
+    </React.Fragment>
+  );
+};
+
+export default Users;
